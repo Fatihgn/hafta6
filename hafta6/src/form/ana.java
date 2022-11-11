8714d9c3f1bee30c885d61e78031ae2df7e9a4ad
ACER <ACER@10.47.247.87>
2022-11-11 17:14:43 +0300

    lab

---
+++ hafta6/src/form/ana.java
@@ -0,0 +1,92 @@
+package form;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ana {
+
+	public static String Maxed(HashMap<String, Integer> puanlar) {
+		int max = 0;
+		String soru = null;
+		for (String i : puanlar.keySet()) {
+			if (puanlar.get(i) > max) {
+				max = puanlar.get(i);
+				soru = i;
+			}
+		}
+		
+		return soru;
+	}
+	
+	public static String Mined(HashMap<String, Integer> puanlar) {
+		int min = 11;
+		String soru = null;
+		for (String i : puanlar.keySet()) {
+			if (puanlar.get(i) < min) {
+				min = puanlar.get(i);
+				soru = i;
+			}
+		}
+		
+		return soru;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		Scanner s2 = new Scanner(System.in);
+		int puan;
+		String istek;
+		HashMap<String, Integer> puanlar = new HashMap<String, Integer>();
+		puanlar.put("kuresel ne kadar onemli?", 0);
+		puanlar.put("ekonomi ne kadar onemli?", 0);
+		puanlar.put("enerji ne kadar onemli?", 0);
+		puanlar.put("teknoloji ne kadar onemli?", 0);
+		puanlar.put("kultur ne kadar onemli?", 0);
+		int girenkisi=0;
+		
+		while (true) {
+			System.out.print("Forma baslamak icin (b) cikmak icin (q)");
+			istek = s.next();
+			if (istek.equals("q")) {
+				System.out.print("en yuksek puan: "+Maxed(puanlar)+ 
+						"aldigi puan :"+ puanlar.get(Maxed(puanlar))+"\n");
+				System.out.print("en dusuk puan: "+Mined(puanlar)+
+						"aldigi puan: "+ puanlar.get(Mined(puanlar))+"\n");
+				
+			}
+				
+			
+			if (istek.equals("b")) {
+				if (girenkisi == 0)
+					girenkisi=1;
+				else {
+					girenkisi--;
+				}
+				
+				for (String i : puanlar.keySet()) {
+					int currentpuan = puanlar.get(i);
+					System.out.println(i+ "aldigi puan ort: "+puanlar.get(i)/girenkisi);
+					puan = s2.nextInt();
+					if (puan > 10 || puan < 0) {
+						System.out.println("lütfen 1 ile 10 arasinda bir deger girin >\n");
+						while (puan > 10 || puan < 0)
+							//System.out.print("olmadi \n");
+							puan = s2.nextInt();
+							continue;
+					}
+					
+					currentpuan += puan;
+					puanlar.put(i, currentpuan);
+					
+				}
+			}
+			
+			
+			girenkisi++;
+		}
+	}
+
+}
